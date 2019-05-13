e65f0efdf097bae12fcd4e48dbe09c5e5fab325b
Victor Schubert <victor.schubert@doctolib.com>
2019-05-13 17:46:53 +0200

    never apologize

--- entry_points.cpp
+++ entry_points.cpp
@@ -3,8 +3,6 @@
 #include "wrapper.hpp"
 #include "vaargs.hpp"
 
-/* I’m so sorry… */
-
 extern "C" {
 #define X(result, name, ...) result name(EV(PARAMS, PP_NARG(magic,## __VA_ARGS__))(__VA_ARGS__)) { \
 	return autoit_function(&autoit_instance::name EV(ARGS, PP_NARG(magic,## __VA_ARGS__))); \
