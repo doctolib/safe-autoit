4b63eddbc21abec39882273dd2813cac2cfb1766
Victor Schubert <victor.schubert@doctolib.com>
2019-05-13 17:53:33 +0200

    use correct path and init library

--- autoit_instance_symbols.cpp
+++ autoit_instance_symbols.cpp
@@ -1,8 +1,12 @@
 #include <windows.h>
 #include "autoit_instance_symbols.hpp"
 
+#define AUTOIT_LIBRARY_NAME "AutoItX3.dll"
+
 autoit_instance_symbols::autoit_instance_symbols(void) {
-	this->handle = LoadLibraryA("toto");
+	this->handle = LoadLibraryA(AUTOIT_LIBRARY_NAME);
+	void __stdcall (*init)(void) = (void __stdcall (*)(void))GetProcAddress(this->handle, "AU3_Init");
+	init();
 	#define X(type, name, params...) this->name = (type __stdcall (*)(params))GetProcAddress(this->handle, #name);
 	# include "autoit_functions.h"
 	#undef X
